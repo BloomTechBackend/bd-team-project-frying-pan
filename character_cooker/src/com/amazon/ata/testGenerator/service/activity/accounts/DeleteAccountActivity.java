042f4a7f6a6c785aab1e243f585c2f6d308defde
Andy Pan <andybpan@gmail.com>
2024-01-25 20:39:09 -0800

    Delete Account Update

--- character_cooker/src/com/amazon/ata/testGenerator/service/activity/accounts/DeleteAccountActivity.java
+++ character_cooker/src/com/amazon/ata/testGenerator/service/activity/accounts/DeleteAccountActivity.java
@@ -1,8 +1,15 @@
 package com.amazon.ata.testGenerator.service.activity.accounts;
 
+import com.amazon.ata.testGenerator.service.converters.ModelConverter;
 import com.amazon.ata.testGenerator.service.dynamodb.dao.AccountDao;
+import com.amazon.ata.testGenerator.service.dynamodb.dao.TermDao;
+import com.amazon.ata.testGenerator.service.dynamodb.dao.TestTemplateDao;
 import com.amazon.ata.testGenerator.service.dynamodb.models.Account;
+import com.amazon.ata.testGenerator.service.dynamodb.models.Term;
+import com.amazon.ata.testGenerator.service.dynamodb.models.TestTemplate;
 import com.amazon.ata.testGenerator.service.exceptions.InvalidAttributeValueException;
+import com.amazon.ata.testGenerator.service.models.TemplateModel;
+import com.amazon.ata.testGenerator.service.models.TermModel;
 import com.amazon.ata.testGenerator.service.models.accounts.requests.DeleteAccountRequest;
 import com.amazon.ata.testGenerator.service.models.accounts.results.DeleteAccountResult;
 import com.amazon.ata.testGenerator.service.util.TestGeneratorServiceUtils;
@@ -12,15 +19,23 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeleteAccountActivity implements RequestHandler<DeleteAccountRequest, DeleteAccountResult> {
     private final Logger log = LogManager.getLogger();
     private final AccountDao accountDao;
+    private final TestTemplateDao testTemplateDao;
+    private final TermDao termDao;
 
     @Inject
-    public DeleteAccountActivity(AccountDao accountDao) {
+    public DeleteAccountActivity(AccountDao accountDao, TestTemplateDao testTemplateDao, TermDao termDao) {
         this.accountDao = accountDao;
+        this.testTemplateDao = testTemplateDao;
+        this.termDao = termDao;
     }
+
+    @Inject
 
     /**
      *
@@ -31,29 +46,41 @@ public class DeleteAccountActivity implements RequestHandler<DeleteAccountReques
     @Override
     public DeleteAccountResult handleRequest(final DeleteAccountRequest request, Context context) {
         log.info("Received DeleteAccountRequest {}", request);
-
         // validate username
         if(!TestGeneratorServiceUtils.isValidUsername(request.getUsername())) {
             throw new InvalidAttributeValueException("Invalid Username!");
         }
-
         // validate password
         if(!TestGeneratorServiceUtils.isValidUsername(request.getPassword())) {
             throw new InvalidAttributeValueException("Invalid Password");
         }
-
         // Populate account primary key
         Account account = accountDao.getAccount(request.getUsername());
-
         // Check password
         if (request.getPassword().equals(account.getPassword())) {
             throw new InvalidAttributeValueException("Incorrect Password");
         }
-
         accountDao.deleteAccount(account);
+
+        List<TestTemplate> templates = testTemplateDao.getTemplateByUserTitle(request.getUsername());
+        List<TemplateModel> templateModels = new ArrayList<>();
+        for (TestTemplate template : templates) {
+            testTemplateDao.deleteTemplate(template);
+            templateModels.add(ModelConverter.toTemplateModel(template));
+        }
+
+        List<Term> terms = termDao.getTermsByUser(request.getUsername());
+        List<TermModel> termModels = new ArrayList<>();
+        for (Term term : terms) {
+            termDao.deleteTerm(term);
+            termModels.add(ModelConverter.toTermModel(term));
+        }
 
         return DeleteAccountResult.builder()
                 .withLogMessage("Account deletion successful")
+                .withUsername(request.getUsername())
+                .withTemplatesDeleted(templateModels)
+                .withTermsDeleted(termModels)
                 .build();
     }
 }
--- character_cooker/src/com/amazon/ata/testGenerator/service/models/accounts/results/DeleteAccountResult.java
+++ character_cooker/src/com/amazon/ata/testGenerator/service/models/accounts/results/DeleteAccountResult.java
@@ -1,18 +1,25 @@
 package com.amazon.ata.testGenerator.service.models.accounts.results;
 
+import com.amazon.ata.testGenerator.service.dynamodb.models.TestTemplate;
+import com.amazon.ata.testGenerator.service.models.TemplateModel;
+import com.amazon.ata.testGenerator.service.models.TermModel;
+
 import java.util.List;
 import java.util.Objects;
 
 public class DeleteAccountResult {
     private String logMessage;
     private String username;
-    private List<String> templatesDeleted;
-    private List<String> termsDeleted;
+    private List<TemplateModel> templatesDeleted;
+    private List<TermModel> termsDeleted;
 
     public DeleteAccountResult() {}
 
     public DeleteAccountResult(Builder builder) {
         this.logMessage = builder.logMessage;
+        this.username = builder.username;
+        this.templatesDeleted = builder.templatesDeleted;
+        this.termsDeleted = builder.termsDeleted;
     }
 
     public String getLogMessage() {
@@ -21,6 +28,33 @@ public class DeleteAccountResult {
 
     public DeleteAccountResult setLogMessage(String logMessage) {
         this.logMessage = logMessage;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public DeleteAccountResult setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public List<TemplateModel> getTemplatesDeleted() {
+        return templatesDeleted;
+    }
+
+    public DeleteAccountResult setTemplatesDeleted(List<TemplateModel> templatesDeleted) {
+        this.templatesDeleted = templatesDeleted;
+        return this;
+    }
+
+    public List<TermModel> getTermsDeleted() {
+        return termsDeleted;
+    }
+
+    public DeleteAccountResult setTermsDeleted(List<TermModel> termsDeleted) {
+        this.termsDeleted = termsDeleted;
         return this;
     }
 
@@ -43,9 +77,27 @@ public class DeleteAccountResult {
 
     public static final class Builder {
         private String logMessage;
+        private String username;
+        private List<TemplateModel> templatesDeleted;
+        private List<TermModel> termsDeleted;
 
         public Builder withLogMessage(String logMessage) {
             this.logMessage = logMessage;
+            return this;
+        }
+
+        public Builder withUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder withTemplatesDeleted(List<TemplateModel> templatesDeleted) {
+            this.templatesDeleted = templatesDeleted;
+            return this;
+        }
+
+        public Builder withTermsDeleted(List<TermModel> termsDeleted) {
+            this.termsDeleted = termsDeleted;
             return this;
         }
 
